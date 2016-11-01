f12f206afeffb8f86de70ca584e966472b1e80cc
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-11-02 00:07:45 +0300

    Task 3

---
+++ LogAnalysis/LogAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using MSUtil;
+
+namespace LogAnalysis
+{
+    public class LogAnalyzer
+    {
+        private readonly LogQueryClass _logQuery = new LogQueryClass();
+
+        private readonly COMTSVInputContextClass _context = new COMTSVInputContextClass
+        {
+            headerRow = false,
+            iTsFormat = "yyyy-MM-dd HH:mm:ss.ffff",
+            iSeparator = "|",
+            nFields = 3
+        };
+
+        private readonly string _filepath;
+
+        public LogAnalyzer(string filepath)
+        {
+            _filepath = filepath;
+        }
+
+        public ILogRecordset RecordCountByLevel
+        {
+            get
+            {
+                var query = $"SELECT Field2 AS LogLevel, COUNT(*) AS RecordCount " +
+                            $"FROM {_filepath} " +
+                            $"GROUP BY Field2";
+
+                return _logQuery.Execute(query, _context);
+            }
+        }
+
+        public ILogRecordset ErrorRecords
+        {
+            get
+            {
+                var query = $"SELECT Field1 AS Date, Field2 AS LogLevel, Field3 AS Message " +
+                            $"FROM {_filepath} " +
+                            $"WHERE Field2 = 'ERROR'";
+
+                return _logQuery.Execute(query, _context);
+            }
+        }
+    }
+}
---
+++ LogAnalysis/LogAnalyzerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogAnalysis
+{
+    [TestClass]
+    public class LogAnalyzerTests
+    {
+        private readonly LogAnalyzer _logAnalyzer;
+
+        public LogAnalyzerTests()
+        {
+            var filepath = Path.Combine(Environment.CurrentDirectory, @"..\..\..\MvcMusicStore\logs\2016-11-01.log");
+            _logAnalyzer = new LogAnalyzer(filepath);
+        }
+
+        [TestMethod]
+        public void RecordCountByLevel()
+        {
+            var recordset = _logAnalyzer.RecordCountByLevel;
+
+            while (!recordset.atEnd())
+            {
+                var record = recordset.getRecord();
+
+                Console.WriteLine(
+                    "Log level: {0} - Record count: {1}",
+                    record.getValue("LogLevel"),
+                    record.getValue("RecordCount"));
+
+                recordset.moveNext();
+            }
+        }
+
+        [TestMethod]
+        public void ErrorRecords()
+        {
+            var recordset = _logAnalyzer.ErrorRecords;
+
+            while (!recordset.atEnd())
+            {
+                var record = recordset.getRecord();
+
+                Console.WriteLine(
+                    "Date: {0} - Log level: {1} - Message: {2}",
+                    record.getValue("Date"),
+                    record.getValue("LogLevel"),
+                    record.getValue("Message"));
+
+                recordset.moveNext();
+            }
+        }
+    }
+}
